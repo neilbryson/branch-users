1c603cae50021d3e5cf1322b64228c2e0985b76e
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2022-07-30 16:39:48 +0800

    chore: remove unnecessary dependencies

--- src/utilities/api.ts
+++
@@ -1,9 +0,0 @@
-import axios, { type AxiosRequestConfig, type AxiosResponse } from 'axios';
-
-export function api<T = Record<string, unknown>>(options: AxiosRequestConfig = {}): Promise<AxiosResponse<T>> {
-  const axiosConfig: AxiosRequestConfig = {
-    baseURL: process.env.REACT_APP_SERVER_BASE_URL,
-    ...options,
-  };
-  return axios.request(axiosConfig);
-}
--- src/views/RootView.tsx
+++ src/views/RootView.tsx
@@ -1,14 +1,11 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { useTheme } from '../hooks/useTheme';
 import store from '../store/configureStore';
 import { NotFoundView } from './NotFoundView';
 import { SampleView } from './SampleView';
 
 export const RootView = () => {
-  useTheme();
-
   return (
     <Provider store={store}>
       <BrowserRouter>
