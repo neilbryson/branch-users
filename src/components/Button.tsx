a12b2f08bddeaaff640e1f59d2ddc6d5647a7aa0
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2022-07-30 21:41:03 +0800

    feat(button): allow extension of classes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,12 +4,12 @@ export type ButtonProps = {
   isPrimary?: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = ({ isPrimary = true, ...props }: ButtonProps) => {
+export const Button = ({ className = '', isPrimary = true, ...props }: ButtonProps) => {
   return (
     <button
       className={`p-2 border rounded disabled:bg-gray-100 disabled:text-gray-500 ${
         isPrimary ? 'bg-blue-400 hover:bg-blue-500' : 'bg-gray-300 hover:bg-gray-400'
-      }`}
+      } ${className}`}
       {...props}
     />
   );
